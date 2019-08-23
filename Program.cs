3528dea1641d461f8210dc67ab7abbe728cf815b
glow98 <40080461+glow98@users.noreply.github.com>
2019-08-22 21:38:29 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using MVC.Model;
+using MVC.View;
+using MVC.Controller;
+
+namespace UseMVCApplication
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            UsersView view = new UsersView();
+ 
+            IList users = new ArrayList();
+
+            users.Add(new User("Vladimir", "Putin",
+                 "122", "Gobernador de Russia",
+                User.SexOfPerson.Male));
+            users.Add(new User("Barack", "Obama",
+                 "123", "Gobernador de USA",
+                 User.SexOfPerson.Male));
+            users.Add(new User("Stephen", "Harper",
+                "124", "Gobernador de Canada",
+                User.SexOfPerson.Male));
+            users.Add(new User("Jean", "Charest",
+                "125", "Gobernador de Quebec",
+                 User.SexOfPerson.Male));
+            users.Add(new User("David", "Cameron",
+                "126", "Gobernador de United Kingdom",
+                User.SexOfPerson.Male));
+            users.Add(new User("Angela", "Merkel",
+                "127", "Gobernador de Alemania",
+                User.SexOfPerson.Female));
+            users.Add(new User("Nikolas", "Sarkozy",
+                "128", "Gobernador de Francia",
+                User.SexOfPerson.Male));
+            users.Add(new User("Silvio", "Berlusconi",
+                "129", "Gobernador de Italia",
+                User.SexOfPerson.Male));
+            users.Add(new User("Yoshihiko", "Noda",
+                "130", "Gobernador de Japon",
+                User.SexOfPerson.Male));
+
+            UsersController controller = new UsersController(view, users);
+            controller.LoadView();
+        }
+    }
+}
---
+++ UsersView.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using MVC.Controller;
+using MVC.Model;
+
+namespace MVC.View
+{
+    public partial class UsersView : Form, IUsersView
+    {
+        public void SetController(UsersController controller)
+        {
+            _controller = controller;
+        }
+
+        public void AddUserToGrid(User usr)
+        {
+            ListViewItem parent;
+            parent = this.grdUsers.Items.Add(usr.ID);
+            parent.SubItems.Add(usr.FirstName);
+            parent.SubItems.Add(usr.LastName);
+            parent.SubItems.Add(usr.Department);
+            parent.SubItems.Add(Enum.GetName(typeof(User.SexOfPerson), usr.Sex));
+        }
+
+        public void UpdateGridWithChangedUser(User usr)
+        {
+            ListViewItem rowToUpdate = null;
+
+            foreach (ListViewItem row in this.grdUsers.Items)
+            {
+                if (row.Text == usr.ID)
+                {
+                    rowToUpdate = row;
+                }
+            }
+
+            if (rowToUpdate != null)
+            {
+                rowToUpdate.Text = usr.ID;
+                rowToUpdate.SubItems[1].Text = usr.FirstName;
+                rowToUpdate.SubItems[2].Text = usr.LastName;
+                rowToUpdate.SubItems[3].Text = usr.Department;
+                rowToUpdate.SubItems[4].Text = Enum.GetName(typeof(User.SexOfPerson), usr.Sex);
+            }
+        }
+
+        public void RemoveUserFromGrid(User usr)
+        {
+
+            ListViewItem rowToRemove = null;
+
+            foreach (ListViewItem row in this.grdUsers.Items)
+            {
+                if (row.Text == usr.ID)
+                {
+                    rowToRemove = row;
+                }
+            }
+
+            if (rowToRemove != null)
+            {
+                this.grdUsers.Items.Remove(rowToRemove);
+                this.grdUsers.Focus();
+            }
+        }
+
+        public string GetIdOfSelectedUserInGrid()
+        {
+            if (this.grdUsers.SelectedItems.Count > 0)
+                return this.grdUsers.SelectedItems[0].Text;
+            else
+                return "";
+        }
+
+        public void SetSelectedUserInGrid(User usr)
+        {
+            foreach (ListViewItem row in this.grdUsers.Items)
+            {
+                if (row.Text == usr.ID)
+                {
+                    row.Selected = true;
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get { return this.txtFirstName.Text; }
+            set { this.txtFirstName.Text = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.txtLastName.Text; }
+            set { this.txtLastName.Text = value; }
+        }
+
+        public string ID
+        {
+            get { return this.txtID.Text; }
+            set { this.txtID.Text = value; }
+        }
+
+
+        public string Department
+        {
+            get { return this.txtDepartment.Text; }
+            set { this.txtDepartment.Text = value; }
+        }
+
+        public User.SexOfPerson Sex
+        {
+            get
+            {
+                if (this.rdMale.Checked)
+                    return User.SexOfPerson.Male;
+                else
+                    return User.SexOfPerson.Female;
+            }
+            set
+            {
+                if (value == User.SexOfPerson.Male)
+                    this.rdMale.Checked = true;
+                else
+                    this.rdFamele.Checked = true;
+            }
+        }
+
+        public bool CanModifyID
+        {
+            set { this.txtID.Enabled = value; }
+        }
+
+
+
+
+    }
+
+
+}
